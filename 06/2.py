9757535e8be6c1b286d54c5e704ed6b6cb733268
MartinHurnak <hurnak.m@gmail.com>
2025-12-15 22:29:49 +0100

    Day 6

---
+++ 06/2.py
@@ -0,0 +1,23 @@
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+from test_run import test_and_run
+
+dirname = os.path.dirname(__file__)
+testfile = os.path.join(dirname, "test.txt")
+inputfile = os.path.join(dirname, "input.txt")
+
+MARKER_LEN = 14 
+
+def main(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+
+    signal = lines[0].strip()
+    for i in range(MARKER_LEN, len(signal)):
+        if len(set(signal[i-MARKER_LEN:i])) == MARKER_LEN:
+            return i
+
+
+EXPECTED_TEST_RESULT = 19 
+test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)

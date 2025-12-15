a764bfc8015eac80f78ff81ba8949edcb42a8352
MartinHurnak <hurnak.m@gmail.com>
2025-12-15 20:07:32 +0100

    Utils

---
+++ dirmaker.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+for i in range(1, 26):
+    dirname = str(i).zfill(2)
+    os.makedirs(dirname, exist_ok=True)
+    for filename in ["1.py", "2.py"]:
+        shutil.copy("template.py", os.path.join(dirname, filename))
+    for filename in ["test.txt", "input.txt"]:
+        with open(os.path.join(dirname, filename), "w"):
+            pass
---
+++ template.py
@@ -0,0 +1,20 @@
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+from test_run import test_and_run
+
+dirname = os.path.dirname(__file__)
+testfile = os.path.join(dirname, "test.txt")
+inputfile = os.path.join(dirname, "input.txt")
+
+def main(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+
+    # TODO implement me
+
+    return None
+
+
+EXPECTED_TEST_RESULT = 0  # TODO change based on test
+test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
